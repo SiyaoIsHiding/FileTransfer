d501cbed60685d76fd89321f27adbdec66c86e35
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2022-11-17 22:04:26 -0800

    hardcode path

--- src/FileClient.java
+++ src/FileClient.java
@@ -1,23 +1,23 @@
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class FileClient {
     public static final int POST_SIZE = 1000;
     public static String f = "received/received1.txt";
     public static void main(String[] args) throws Exception{
-        Socket socket = new Socket("127.0.0.1", 5000);
+        Socket socket = new Socket("127.0.0.1", 80);
         byte[] contents = new byte[POST_SIZE];
 
         InputStream in = socket.getInputStream();
-        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(f));
-        int received;
-        while ((received = in.read()) != -1){
-            out.write(contents, 0, received);
-            System.out.println(contents);
+        InputStreamReader reader = new InputStreamReader(in, "ASCII");
+        StringBuilder received = new StringBuilder();
+        for (int c = reader.read(); c != -1; c=reader.read()){
+            received.append((char) c);
         }
-        out.flush();
+        System.out.println(received.toString());
         socket.close();
         System.out.println("File received");
 
--- src/FileHTTPServer.java
+++ src/FileHTTPServer.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,22 +8,14 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SingleFileHTTPServer {
-    private final byte[] content;
-    private final byte[] header;
+public class FileHTTPServer {
     private final int port;
     private final String encoding;
 
 
-    public SingleFileHTTPServer(byte[] content, int port, String encoding, String mimeType) {
-        this.content = content;
+    public FileHTTPServer(int port, String encoding) {
         this.port = port;
         this.encoding = encoding;
-        String header = "HTTP/1.0 200 OK\r\n" +
-                "Server: OneFile 2.0\r\n" +
-                "Content-length: " + this.content.length + "\r\n" +
-                "Content-type: " + mimeType + "; charset=" + encoding + "\r\n\r\n";
-        this.header = header.getBytes(Charset.forName("US-ASCII"));
     }
 
     public void start(){
@@ -33,11 +23,10 @@ public class SingleFileHTTPServer {
         try (ServerSocket server = new ServerSocket(this.port)){
             System.out.println("Server listening on port " + server.getLocalPort());
             System.out.println("Data to be sent");
-            System.out.println(new String(this.content, encoding));
             while(true){
                 try{
                     Socket connection = server.accept();
-                    pool.submit(new HTTPHandler(connection));
+                    pool.submit(new SendSingle(connection));
                 }catch(IOException e){
                     e.printStackTrace();
                 }catch(RuntimeException e){
@@ -49,20 +38,21 @@ public class SingleFileHTTPServer {
         }
     }
 
-    private class HTTPHandler implements Callable<Void> {
+    private class SendSingle implements Callable<Void> {
         private final Socket connection;
 
-        private HTTPHandler(Socket connection) {
+        private SendSingle(Socket connection) {
             this.connection = connection;
         }
 
         @Override
         public Void call() throws Exception {
             try{
+                Path path = Paths.get("public/data1.txt");
+                byte[] data = Files.readAllBytes(path);
                 OutputStream out = new BufferedOutputStream(connection.getOutputStream());
                 InputStream in = new BufferedInputStream(connection.getInputStream());
-                out.write(header);
-                out.write(content);
+                out.write(data);
                 out.flush();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -77,14 +67,9 @@ public class SingleFileHTTPServer {
         int port = 80;
         String encoding = "US-ASCII";
         try {
-            Path path = Paths.get("public/data1.txt");
-            byte[] data = Files.readAllBytes(path);
-
-            String contentType = URLConnection.getFileNameMap().getContentTypeFor("public/data1.txt");
-            SingleFileHTTPServer server  = new SingleFileHTTPServer(data, port, encoding, contentType);
+            FileHTTPServer server  = new FileHTTPServer(port, encoding);
             server.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+
         } catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Usage: java SingleFileHTTPServer.java filename port encoding");
         }
